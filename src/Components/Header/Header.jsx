8c28a7a6747cd03d733d775757b5d4316f4adf45
Hafeez Khan <khan3344kpk@gmail.com>
2024-11-16 20:27:00 +0500

    change logotext

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="flex justify-between items-center bg-[--main] p-4 lg:px-10 h-[66px]">
     <div className="flex items-center space-x-2">
       <img className="h-12" src={logo} alt="" />
-      <span className="hidden sm:inline  text-white font-bold text-3xl font-roboto">BetPay</span>
+      <span className="hidden sm:inline  text-white font-bold text-3xl font-roboto">BetPayyyyy</span>
     </div>
     <div className="text-white font-bold text-3xl font-roboto sm:hidden">
       BetPay
