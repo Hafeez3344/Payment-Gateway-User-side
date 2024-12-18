3ff8d60276542e8ab1457f85232797b784cdd372
Hafeez3344 <khan3344kpk@gmail.com>
2024-12-18 16:38:30 +0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const BACKEND_URL = "http://localhost:8888"
 
 export const fn_getBanksByTabApi = async (tab) => {
+
     try {
         const response = await axios.get(`${BACKEND_URL}/bank/user?accountType=${tab}&website=${window.location.origin}`);
         if (response?.status === 200) {
@@ -13,7 +14,7 @@ export const fn_getBanksByTabApi = async (tab) => {
     } catch (error) {
         console.log("fn_getBanksByTabApi ", error);
         if (error?.status === 400) {
-            return { status: false, message: error?.response?.data?.message || "Something went wrong" }
+            return { status: false, message: error?.response?.data?.message || "Something went wrong" };
         }
         return { status: false, message: "Network error" };
     }
